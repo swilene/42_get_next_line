b2c0b9b1aa98ce40a815ef3c29846e241be2edda
swilene <102738831+swilene@users.noreply.github.com>
2022-06-15 19:42:45 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,121 +6,11 @@
 /*   By: saguesse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 09:52:23 by saguesse          #+#    #+#             */
-/*   Updated: 2022/06/14 17:23:26 by saguesse         ###   ########.fr       */
+/*   Updated: 2022/06/15 19:39:46 by saguesse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-size_t	ft_strlen(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strdup(char *src)
-{
-	int		i;
-	char	*dup;
-
-	i = 0;
-	dup = (char *)malloc(sizeof(*dup) * (ft_strlen(src) + 1));
-	if (!dup)
-		return (NULL);
-	while (src[i])
-	{
-		dup[i] = src[i];
-		i++;
-	}
-	dup[i] = '\0';
-	return (dup);
-}
-
-void	ft_bzero(void *s, size_t n)
-{
-	char		*str;
-	size_t		i;
-
-	i = 0;
-	str = s;
-	while (i != n)
-	{
-		str[i] = '\0';
-		i++;
-	}
-}
-
-char	*write_line(char *line, int i)
-{
-	int		j;
-	char	*str;
-
-	j = 0;
-	str = (char *)malloc(sizeof(*str) * i + 1);
-	if (!str)
-		return (NULL);
-	while (j < i)
-	{
-		str[j] = line[j];
-		j++;
-	}
-	free(line);
-	return (str[j] = '\0', str);
-}
-
-char	*write_buf(char *line, char *buf)
-{
-	int	i;
-	int	len;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (line[i] && line[i] != '\n')
-		i++;
-	i++;
-	len = i;
-	while (line[i])
-	{
-		buf[j] = line[i];
-		i++;
-		j++;
-	}
-	line = write_line(line, len);
-	return (line);
-}
-
-char	*ft_strjoin(char *line, char *buf, int ret)
-{
-	int		i;
-	int		j;
-	char	*str;
-
-	if (line == NULL)
-		return (buf);
-	i = ft_strlen(line);
-	j = 0;
-	str = (char *)malloc(sizeof(*str) * (i + ret) + 1);
-	if (!str)
-		return (NULL);
-	while (line[j])
-	{
-		str[j] = line[j];
-		j++;
-	}
-	j = 0;
-	while (buf[j])
-	{
-		str[i + j] = buf[j];
-		j++;
-	}
-	str[i + j] = '\0';
-	return (str);
-}
 
 char	*ft_strchr(const char *s)
 {
@@ -135,35 +25,61 @@ char	*ft_strchr(const char *s)
 	return (NULL);
 }
 
+int	ft_read_fd(int fd, char **l, char *buf)
+{
+	int		ret;
+	char	*line;
+
+	line = *l;
+	ret = read(fd, buf, BUFFER_SIZE);
+	if (ret == -1)
+		return (1);
+	if (ret == 0)
+		return (2);
+	buf[ret] = '\0';
+	if (line == NULL)
+	{
+		line = ft_strdup(buf);
+		if (line == NULL)
+			return (1);
+	}
+	else
+	{
+		line = ft_strjoin(line, buf, ret);
+		if (line == NULL)
+			return (1);
+	}
+	*l = line;
+	return (0);
+}
+
 char	*get_next_line(int fd)
 {
-	int			ret;
 	static char	buf[BUFFER_SIZE + 1];
 	char		*line;
+	int			i;
 
-	if (fd == -1)
+	i = 0;
+	line = NULL;
+	if (fd < 0)
 		return (NULL);
-	if (buf[0] != '\0')
-		line = ft_strdup(buf);
-	else
-		line = NULL;
-	while (ft_strchr(buf) == NULL)
+	if (*buf)
 	{
-		ret = read(fd, buf, BUFFER_SIZE);
-		if (ret == -1 || ret == 0)
-			return (NULL);
-		buf[ret] = '\0';
+		line = ft_strdup(buf);
 		if (line == NULL)
-			line = ft_strdup(buf);
-		else
-			line = ft_strjoin(line, buf, ret);
+			return (NULL);
 	}
+	while (ft_strchr(buf) == NULL && i == 0)
+		i = ft_read_fd(fd, &line, buf);
+	if (i == 1)
+		return (NULL);
 	ft_bzero(buf, BUFFER_SIZE + 1);
-	line = write_buf(line, buf);
+	if (i == 0)
+		write_buf(line, buf);
 	return (line);
 }
 
-int	main()
+/*int	main()
 {
 	int		fd;
 	char	*line;
@@ -183,4 +99,4 @@ int	main()
 		return (1);
 	}
 	return (0);
-}
+}*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: saguesse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 15:51:21 by saguesse          #+#    #+#             */
-/*   Updated: 2022/06/07 15:56:52 by saguesse         ###   ########.fr       */
+/*   Updated: 2022/06/15 14:08:47 by saguesse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,10 @@
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(char *src);
+void	ft_bzero(void *s, size_t n);
+void	write_buf(char *line, char *buf);
+char	*ft_strjoin(char *line, char *buf, int ret);
 
-# endif
+#endif
